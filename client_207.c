92250a00549cf6032d19ad3c38cf6101f63762c6
Paxshal <mehtapaxshal@gmail.com>
2016-01-06 20:37:19 -0800

    CMPE 207 Final Project
    Smart Assistance System. (step towards IOT)

---
+++ client_207.c
@@ -0,0 +1,338 @@
+#include <stdio.h>
+#include <gtk/gtk.h>		//for GTK widget GUI
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <glib.h>		//include GTK libraries
+
+
+int clientSocket;
+struct sockaddr_in serverAddr;
+socklen_t addr_size;
+char rqust[20],ack[20],buffer[30];
+static const int BUFFER_SIZE= 2*1024;
+int flag=0;
+pthread_t thread;
+pthread_attr_t attr;
+GtkWidget *window, *button, *table;
+pthread_t thread1;
+pthread_attr_t attr1;
+
+/****************video receive function**********************/
+
+void video_read(int clientSocket)
+{
+	FILE *fps;	
+	int bytesReceived;
+	fps=fopen("vod.h264","w");
+	if(fps)
+	{
+		char buff[BUFFER_SIZE];
+		while(1)
+		{	
+			bytesReceived=recv(clientSocket,buff,sizeof (buff),0);
+			if (bytesReceived < 0) 
+				perror("recv");
+			if (bytesReceived == 0) 
+				break;   
+			printf("The no. of bytes received of n is %d\n",bytesReceived);
+			if(fwrite(buff,1,bytesReceived,fps)!=(size_t) bytesReceived)
+			{
+				perror("fwrite\n");
+				break;
+			}
+			else
+			{			
+			flag=1;
+			}
+		}	
+		fclose(fps);
+	}
+
+	pthread_exit(NULL);
+}
+
+/****************video display function**********************/
+
+void VLCservice()
+{
+
+	while(1)
+	{
+	if(flag==1)
+	break;	
+	else		//flag=0;
+	sleep(1);	
+	}	
+	
+	system("vlc --rate=0.52 --no-playlist-autostart -vvv vod.h264 --tcp-caching =300 vlc://quit");	
+	
+	pthread_exit(NULL);
+}
+
+/****************send request for light**********************/
+
+static void lightOn(GtkWidget *widget, gpointer data)
+{	
+	send(clientSocket,"A",sizeof(char),0);	
+	recv(clientSocket,buffer,sizeof(buffer),0);
+	printf("Server reply: %s\n",buffer);
+	
+}
+
+static void lightOff(GtkWidget *widget, gpointer data)
+{
+	send(clientSocket,"B",sizeof(char),0);	
+	recv(clientSocket,buffer,sizeof(buffer),0);
+	printf("Server reply: %s\n",buffer);
+}
+
+/**************send request for LED (alert)******************/
+
+static void ledOn(GtkWidget *widget, gpointer data)
+{	
+	send(clientSocket,"H",sizeof(char),0);	
+	recv(clientSocket,buffer,sizeof(buffer),0);
+	printf("Server reply: %s\n",buffer);
+	
+}
+
+static void ledOff(GtkWidget *widget, gpointer data)
+{
+	send(clientSocket,"I",sizeof(char),0);	
+	recv(clientSocket,buffer,sizeof(buffer),0);
+	printf("Server reply: %s\n",buffer);
+}
+
+/****************send request for motor**********************/
+
+static void motorOn(GtkWidget *widget, gpointer data)
+{
+	send(clientSocket,"E",sizeof(char),0);		
+	recv(clientSocket,buffer,sizeof(buffer),0);
+	printf("Server reply: %s\n",buffer);
+}
+
+static void motorOff(GtkWidget *widget, gpointer data)
+{
+	send(clientSocket,"F",sizeof(char),0);	
+	recv(clientSocket,buffer,sizeof(buffer),0);
+	printf("Server reply: %s\n",buffer);
+
+}
+
+/****************send request for video**********************/
+
+static void video(GtkWidget *widget, gpointer data)
+{
+
+
+			pthread_attr_init(&attr);
+			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);	
+
+			pthread_attr_init(&attr1);
+			pthread_attr_setdetachstate(&attr1, PTHREAD_CREATE_JOINABLE);	
+
+	send(clientSocket,"C",sizeof(char),0);	
+
+	if(pthread_create(&thread, &attr, (void * (*) (void *))VLCservice,NULL) < 0)
+		perror("pthread_create error\n");
+
+	if(pthread_create(&thread1, &attr1, (void * (*) (void *))video_read,(int*)clientSocket) < 0)
+		perror("pthread_create error\n");
+	
+pthread_join(thread1,NULL);
+pthread_join(thread,NULL);
+
+}
+
+/****************send request for status**********************/
+
+static void Status(GtkWidget *widget, gpointer data)
+{
+
+     send(clientSocket,"G",sizeof(char),0);  
+
+	GtkWidget *dialog;
+
+    dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK,"%s", message);
+
+    gtk_window_set_title(GTK_WINDOW(dialog), "Alert!");
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);*/
+	
+
+        recv(clientSocket,buffer,sizeof(buffer),0);
+        char *message = buffer;
+
+    dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK,"%s", message);
+
+    gtk_window_set_title(GTK_WINDOW(dialog), "Status");
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+
+    printf("Server reply: %s\n",buffer);
+        
+}
+
+/****************handle delete event for GUI********************/
+
+static gboolean delete_event(GtkWidget *widget, GdkEvent *event,
+	gpointer data)
+{
+	g_print("delete event occurred\n");
+	
+	close(clientSocket);
+	exit (0); /* return FALSE, instead, to destroy the main window */
+}
+
+/****************destroy event for GUI**********************/
+
+static void destroy(GtkWidget *widget, gpointer data )
+{
+	
+	gtk_main_quit();
+}
+
+/********************main function**************************/
+
+int main(int argc, char *argv[])
+{
+	
+	while(1)
+	{
+	char buffer[1024];
+	int i=0;
+
+	int port=atoi(argv[2]);
+	
+	if(argc <=2)
+	{
+		printf("missing parameters\n");
+		return 1;
+	}	
+	
+	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  
+	if(clientSocket == -1)
+	printf("Socket not created\n");
+
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+	memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));  
+
+        addr_size = sizeof(serverAddr);
+	
+  
+        if(connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size) < 0)
+        {
+                perror("Connection error\n");
+                return;     
+        }
+
+	
+
+	gtk_init(&argc, &argv);
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	g_signal_connect(G_OBJECT(window), "delete_event",
+		G_CALLBACK(delete_event), NULL);
+	g_signal_connect(G_OBJECT(window), "destroy",G_CALLBACK(destroy), NULL);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+
+	table=gtk_table_new(3,3,TRUE);
+
+	gtk_container_add(GTK_CONTAINER(window), table);
+
+	button = gtk_button_new_with_label("Light Turn ON");
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(lightOn), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+	
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,0,1,0,1);	
+
+	button = gtk_button_new_with_label("Light Turn OFF");
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(lightOff), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+		
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,2,3,0,1);	
+
+	button = gtk_button_new_with_label("Motor Turn ON");
+	
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(motorOn), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+		
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,0,1,1,2);	
+
+	button = gtk_button_new_with_label("Motor Turn OFF");
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(motorOff), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+		
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,2,3,1,2);	
+
+	button = gtk_button_new_with_label("Led ON");
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(ledOn), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+		
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,0,1,2,3);
+
+	button = gtk_button_new_with_label("Led OFF");
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(ledOff), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+		
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,2,3,2,3);
+
+	button = gtk_button_new_with_label("Status");
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(Status), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+		
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,1,2,3,4);	
+
+	button = gtk_button_new_with_label("Video");
+	g_signal_connect(G_OBJECT(button), "clicked",
+		G_CALLBACK(video), NULL);
+	g_signal_connect_swapped(G_OBJECT(button), "clicked",
+		G_CALLBACK(gtk_widget_destroy), G_OBJECT(window));
+	
+
+	gtk_table_attach_defaults(GTK_TABLE(table),button,1,2,4,5);	
+	
+	gtk_table_set_row_spacings(GTK_TABLE(table),10);
+
+	gtk_widget_show_all(window);
+	
+	gtk_main();
+
+	}
+	return 0;
+}
---
+++ server_207.c
@@ -0,0 +1,282 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <wiringPi.h>       //For Raspberry PI
+#include <pthread.h>
+
+#define ERROR -1
+#define MAX_CLIENTS 5
+
+
+
+/****************Defining COnstant***************/
+char s;
+static const int BUFFER_SIZE=2*1024;
+int i,c,d,j,systemflag,bytesRead,flag=0,flag1=0,flag2=0,flag3=0,k=0,m=528;
+FILE* fp;
+
+/***************Function to Turn on the camera****************/
+
+void cameraOn(void* newSock)
+		{
+			
+
+			
+			flag=1;
+                        printf("start Recording\n");
+			
+			system("raspivid -o project.h264 -t 30000");
+			
+			printf("Stopped Recording\n");
+			flag=0;
+			pthread_exit(NULL);
+		}
+
+
+/***************Function to handle the Electrical appliances and to know current status****************/
+
+
+void DeviceHandle(int newsockfd)
+	{
+	int n;
+	wiringPiSetup ();
+	pinMode (0, OUTPUT) ;
+	pinMode (5, OUTPUT);
+	pinMode (7, OUTPUT);
+	recv(newsockfd,&s,sizeof s,0);
+        printf("Command received from client\n");
+	
+	switch(s)
+	{
+
+case 'A':
+                /**********Turning light on*********/
+                printf("Light ON\n");
+		flag1=1;
+		digitalWrite(7,HIGH);
+		n=send(newsockfd,"Light On\n",30,0);
+		break;
+case 'B':
+                /**********Turning light off*********/
+                printf("Light OFF\n");
+                flag1=0;
+		digitalWrite(7,LOW);
+	
+		n=send(newsockfd,"Light Off\n",30,0);
+		break;
+
+case 'C':
+{
+                pthread_t thread;
+                pthread_attr_t attr;
+                pthread_attr_init(&attr);
+                pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+
+                char buffer[BUFFER_SIZE];
+		
+
+		if(pthread_create(&thread,&attr,(void *(*)(void *))cameraOn,(void*)&newsockfd)<0)
+		{
+			perror("pthread_create error\n");
+		} 
+		
+		FILE *fps;
+		sleep(3);
+		fps=fopen("project.h264","r");
+		if(fps)
+		{
+			printf("After fopen\n");
+			if(flag==1)
+			{
+                            while(1)
+                            {
+                                printf("Reading the file\n");
+				bytesRead = fread(buffer,1,sizeof(buffer),fps);
+		
+                                if (bytesRead <= 0) break;
+              
+				int sendbytes=send(newsockfd,buffer,bytesRead,0);
+				
+                            }
+
+			}
+		printf("About to close file\n");
+
+		fclose(fps);			
+		}
+		
+		close(newsockfd);
+		pthread_join(thread,NULL);
+                printf("File is closed\n");
+}
+                break;
+
+
+case 'E':
+                /**********Turning Motor on*********/
+		flag2=1;
+		digitalWrite(0,HIGH);
+		
+		n=send(newsockfd,"Motor On\n",30,0);
+		break;
+
+case 'F':
+                /**********Turning Motor off*********/
+		flag2=0;
+		digitalWrite(0,LOW);
+		
+		n=send(newsockfd,"Motor Off\n",30,0);
+		break;
+case 'H':
+                /**********Turning buzzer on as an alarm indication*********/
+                flag3=1;
+		digitalWrite (5, HIGH) ;
+				
+		n=send(newsockfd,"led On\n",30,0);
+		break;
+	
+case 'I':
+                /**********Turning buzzer off*********/
+                flag3=0;
+		digitalWrite (5, LOW) ;		
+		n=send(newsockfd,"led Off\n",30,0);
+		break;
+case 'G':
+                /**********Status of all the appliances*********/
+
+		if((flag1==1) && (flag2==1) && (flag3==1))
+		{
+                    n=send(newsockfd,"LightON\tMotorOn\tLedON\n",30,0);
+		
+		}
+
+                else if(flag1==0 && flag2==0 && flag3==0)
+		{
+                    n=send(newsockfd,"Lightoff\tMotoroff\tLedOff\n",30,0);
+		}
+
+                else if(flag1==1 && flag2==0 &7 && flag3==0)
+		{
+                    n=send(newsockfd,"LightOn\tMotorOff\tLedOff\n",30,0);
+		} 		
+
+                else if(flag1==0 && flag2==1 && flag3==0)
+                {
+                    n=send(newsockfd,"LightOff\tMotorOn\tLedOff\n",30,0);
+                }
+
+                else if(flag1==0 && flag2==1 && flag3==1)
+                {
+                    n=send(newsockfd,"LightOff\tMotorOn\tLedOn\n",30,0);
+                }       
+
+                else if(flag1==1 && flag2==0 && flag3==1)
+                {
+                    n=send(newsockfd,"LightOn\tMotorOff\tLedOn\n",30,0);
+                }
+
+                else if(flag1==0 && flag2==0 && flag3==1)
+                {
+                    n=send(newsockfd,"LightOff\tMotorOff\tLedOn\n",30,0);
+                }
+
+                else if(flag1==1 && flag2==1 && flag3==0)
+                {
+                    n=send(newsockfd,"LightOn\tMotorOn\tLedOff\n",30,0);
+                }
+
+                else
+                    n=send(newsockfd,"redlightOn\n",30,0);
+
+                break;
+
+
+}
+
+}
+	
+
+int main(int argc, char* argv[])
+
+{
+
+int sockfd, newsockfd, portno;
+     socklen_t clilen;
+int n;
+struct sockaddr_in serv_addr, cli_addr;
+
+pthread_t thread1;
+pthread_attr_t attr1;
+pthread_attr_init(&attr1);
+pthread_attr_setdetachstate(&attr1,PTHREAD_CREATE_JOINABLE);    
+
+
+     if (argc < 2) 
+     {
+         printf("ERROR, You have entered wrong Input.Please try again...\n");
+         exit(1);
+     }
+
+     /********Socket*********/
+
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0)
+     printf("ERROR in opening socket");
+
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+     serv_addr.sin_port = htons(portno);
+
+     /********Binding*********/
+
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0)
+              printf("ERROR in binding Socket");
+/********Listen*********/
+
+
+     if((listen(sockfd,MAX_CLIENTS))==ERROR)
+     {
+             perror("Error in Listen:");
+             exit(-1);
+
+     }
+
+
+     clilen = sizeof(cli_addr);
+     printf("Server is Listening...\n");
+while(1)
+{ 
+
+    /********Accept*********/
+
+    if((newsockfd = accept(sockfd,(struct sockaddr*)&cli_addr,&clilen))==ERROR)
+
+        {
+                perror("Error in Accept:");
+                exit(-1);
+        }
+
+
+
+   
+        if(pthread_create(&thread1,&attr1,(void *(*)(void *))DeviceHandle,(int*)newsockfd)<0)
+		{
+			perror("pthread_create error\n");
+		} 
+		
+}
+     close(newsockfd);
+     close(sockfd);
+
+     return 0;
+}
+
+
